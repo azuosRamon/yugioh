9d2eb657478519a3b3ff77438a56ec168d4dbdc8
azuosRamon <167565029+azuosRamon@users.noreply.github.com>
2024-10-01 14:41:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -50,10 +50,10 @@ function atualizar_cartas(url){
         imagem_logo.addEventListener("click", () => {
             cartas_inicio()
         })
-        resetar_form.onclick = () =>{
+        resetar_form.addEventListener("click", () =>{
             cartas_inicio();
             procurar_p.innerHTML = "Utilize um dos campos para pesquisar!";
-        }
+        })
         function cartas_inicio(){
             section_cartas.innerHTML = "";
             limite_cartas_tela = 0;
